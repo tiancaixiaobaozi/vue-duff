b2ec3f566c2499fae7dae73f5d407d8970f821fb
苏佳 <1668488551@qq.com>
2020-04-07 09:56:25 +0800

    测试图片资源

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,13 @@
         style="width: 100px; height: 100px"
       />
     </div>
+    <img
+      style="width: 200px;height: 200px"
+      src="https://api.imjad.cn/interface/img/imagefilter.php?
+      text=%E6%AD%A4%E5%86%85%E5%AE%B9%E4%B8%8D%E9%80%82%E5%90%88%E6%89%80%E6%9C%89%E5%B9%B4%E9%BE%84%E6%AE%B5%5cn%E6%88%96%E4%B8%8D%E5%AE%9C%E5%9C%A8%E5%B7%A5%E4%BD%9C%E6%9C%9F%E9%97%B4%E8%AE%BF%E9%97%AE
+      &url=https://api.imjad.cn/interface/img/PixivProxy.php?url=https://i.pximg.net/c/540x540_70/img-master/img/2020/03/29/22/15/48/80436956_p0_master1200.jpg"
+      alt="123"
+    >
   </div>
 </template>
 
@@ -25,7 +32,7 @@ export default {
   data () {
     return {
       name: '张三',
-      dataSource: ''
+      dataSource: []
     }
   },
   mounted () {
@@ -36,15 +43,15 @@ export default {
       const params = {
         type: 'rank'
       }
+      this.dataSource = []
       let res = await getData(params)
       this.dataSource = res.data.illusts
       console.log(res)
     },
     getImages (_url) {
       if (_url !== undefined) {
-        let _u = _url.substring(20)
-        // return 'https://images.weserv.nl/?url=' + _u
-        return '/img/' + _u
+        // let _u = _url.substring(8)
+        return 'https://api.imjad.cn/interface/img/PixivProxy.php?url=' + _url
       }
     }
   }
--- vue.config.js
+++ vue.config.js
@@ -38,14 +38,6 @@ module.exports = {
         pathRewrite: { // 重写路径 比如'/api/aaa/ccc'重写为'/aaa/ccc'
           '^/api': ''
         }
-      },
-      '/img': {
-        target: 'https://i.pximg.net/', // API服务器的地址
-        ws: true, // 代理websockets
-        changeOrigin: true, // 虚拟的站点需要更管origin
-        pathRewrite: { // 重写路径 比如'/api/aaa/ccc'重写为'/aaa/ccc'
-          '^/img': ''
-        }
       }
     }
   },
